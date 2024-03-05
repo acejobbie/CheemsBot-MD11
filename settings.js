297ff06bb25fc28ad86cbc4e0b07a86a5398835a
Ace <121753552+acejobbie@users.noreply.github.com>
2024-03-05 15:04:40 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,24 +14,24 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: acejobbie98" //ur yt chanel name
+global.socialm = "GitHub: acejobbie" //ur github or insta name
+global.location = "Kenya, Nakuru, Naks" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = '𒋨🏴‍☠️⃝𝘼𝘾𝙀☠️𝘽𝙊𝙏⃝𒋨🉑' //ur bot name
+global.ownernumber = '254726856795' //ur owner number
+global.ownername = '🏴‍☠️⃝🅐ϲԑ 𝚔𝖎𝐫қ𖤍🔥' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
+global.themeemoji = '🉑'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
+global.botscript = 'https://github.com/acejobbie/CheemsBot-MD11' //script link
+global.packname = "𝙸 αɱ Ⓐϲе™⟦𝟭⟧"
+global.author = "🏴‍☠️⃝🅐ϲԑ 𝚔𝖎𝐫қ𖤍🔥\n\n+254726856795"
 global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.xprefix = '#'
+global.premium = ["254726856795"] // Premium User
 global.hituet = 0
 
 //bot sett
